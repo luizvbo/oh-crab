76b505890b73bee87d00a452190fae4c443e0532
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2023-12-15 16:56:02 +0100

    Apply clippy

--- src/rules/history.rs
+++ src/rules/history.rs
@@ -4,22 +4,20 @@ use crate::shell::Shell;
 use crate::utils::{get_close_matches, get_valid_history_without_current};
 
 pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&Box<dyn Shell>>) -> bool {
-    get_close_matches(
+    !get_close_matches(
         &command.script,
-        get_valid_history_without_current(&command, system_shell.unwrap())
+        get_valid_history_without_current(command, system_shell.unwrap())
             .iter()
             .map(|s| s.as_str())
             .collect::<Vec<&str>>()
             .as_slice(),
-    )
-    .len()
-        > 0
+    ).is_empty()
 }
 
 pub fn get_new_command(command: &CrabCommand, system_shell: Option<&Box<dyn Shell>>) -> Vec<String> {
     get_close_matches(
         &command.script,
-        get_valid_history_without_current(&command, system_shell.unwrap())
+        get_valid_history_without_current(command, system_shell.unwrap())
             .iter()
             .map(|s| s.as_str())
             .collect::<Vec<&str>>()
