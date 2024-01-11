91054df9f5054660ae09a9d1cdf1999926142cbf
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-01-11 19:13:38 -0300

    Add the main file

---
+++ src/rules/brew_install.rs
@@ -0,0 +1,120 @@
+use crate::{cli::command::CrabCommand, shell::Shell};
+use regex::Regex;
+
+use super::{get_new_command_without_sudo, match_without_sudo, Rule};
+
+fn get_suggestion(str: String) -> Vec<String> {
+    str.replace(" or ", ", ")
+        .split(", ")
+        .map(|s| s.to_string())
+        .collect()
+}
+
+pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
+    if let Some(stdout) = &command.stdout {
+        command.script.contains("install")
+            && stdout.contains("No available formula")
+            && stdout.contains("Did you mean")
+    } else {
+        false
+    }
+}
+
+pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
+    let re = Regex::new(
+        "Warning: No available formula with the name \"(?:[^\"]+)\". Did you mean (.+)\\?",
+    )
+    .unwrap();
+    let caps = re.captures(&command.stdout.unwrap()).unwrap();
+    let suggestions = get_suggestion(caps.get(1).map_or("", |m| m.as_str()).to_owned());
+    suggestions
+        .iter()
+        .map(|formula| format!("brew install {}", formula))
+        .collect()
+}
+
+pub fn get_rule() -> Rule {
+    Rule::new(
+        "brew_install".to_owned(),
+        None,
+        None,
+        None,
+        match_rule,
+        get_new_command,
+        None,
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{get_new_command, match_rule};
+    use crate::cli::command::CrabCommand;
+
+    macro_rules! parameterized_match_rule_tests {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (script, stdout) = $value;
+                    let mut command = CrabCommand::new(
+                                script.to_owned(),
+                                Some(stdout.to_owned()),
+                                None
+                            );
+                    assert!(match_rule(&mut command, None));
+                }
+            )*
+        }
+    }
+
+    macro_rules! parameterized_unmatch_rule_tests {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (script, stdout) = $value;
+                    let mut command = CrabCommand::new(
+                                script.to_owned(),
+                                Some(stdout.to_owned()),
+                                None
+                            );
+                    assert!(!match_rule(&mut command, None));
+                }
+            )*
+        }
+    }
+
+    macro_rules! parameterized_get_new_command_tests {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (script, stdout, expected) = $value;
+                    let mut command = CrabCommand::new(
+                                script.to_owned(),
+                                Some(stdout.to_owned()),
+                                None
+                            );
+                    assert_eq!(get_new_command(&mut command, None)[0], expected);
+                }
+            )*
+        }
+    }
+
+    parameterized_match_rule_tests! {
+        match_rule_1: ("cd foo", "cd: foo: No such file or directory"),
+        match_rule_2: ("cd foo/bar/baz", "cd: foo: No such file or directory"),
+        match_rule_3: ("cd foo/bar/baz", "cd: can't cd to foo/bar/baz"),
+        match_rule_4: ("cd /foo/bar/", "cd: The directory \"/foo/bar/\" does not exist"),
+    }
+
+    parameterized_unmatch_rule_tests! {
+        unmatch_rule_1: ("cd foo", ""),
+        unmatch_rule_2: ("", ""),
+    }
+
+    parameterized_get_new_command_tests! {
+        get_new_command_1: ("cd foo", "", "mkdir -p foo && cd foo"),
+        get_new_command_2: ("cd foo/bar/baz", "", "mkdir -p foo/bar/baz && cd foo/bar/baz"),
+    }
+}
