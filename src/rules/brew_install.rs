356e2fe2917eed9d4bc368ed22909e7ce2105de1
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-01-16 16:12:10 -0300

    Add unit test
    .

--- src/rules/brew_install.rs
+++ src/rules/brew_install.rs
@@ -3,7 +3,7 @@ use regex::Regex;
 
 use super::{get_new_command_without_sudo, match_without_sudo, Rule};
 
-fn get_suggestion(str: String) -> Vec<String> {
+fn get_suggestions(str: String) -> Vec<String> {
     str.replace(" or ", ", ")
         .split(", ")
         .map(|s| s.to_string())
@@ -25,8 +25,9 @@ pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shel
         "Warning: No available formula with the name \"(?:[^\"]+)\". Did you mean (.+)\\?",
     )
     .unwrap();
-    let caps = re.captures(&command.stdout.unwrap()).unwrap();
-    let suggestions = get_suggestion(caps.get(1).map_or("", |m| m.as_str()).to_owned());
+    let stdout = &command.stdout.as_ref().unwrap();
+    let caps = re.captures(stdout).unwrap();
+    let suggestions = get_suggestions(caps.get(1).map_or("", |m| m.as_str()).to_owned());
     suggestions
         .iter()
         .map(|formula| format!("brew install {}", formula))
@@ -47,11 +48,20 @@ pub fn get_rule() -> Rule {
 
 #[cfg(test)]
 mod tests {
-    use super::{get_new_command, match_rule};
+    use super::{get_new_command, get_suggestions, match_rule};
     use crate::cli::command::CrabCommand;
 
+    const BREW_NO_AVAILABLE_FORMULA_ONE: &str =
+        "Warning: No available formula with the name \"giss\". Did you mean gist?";
+    const BREW_NO_AVAILABLE_FORMULA_TWO: &str = "Warning: No available formula with the name \"elasticserar\". Did you mean elasticsearch or elasticsearch@6?";
+    const BREW_NO_AVAILABLE_FORMULA_THREE: &str =
+        "Warning: No available formula with the name \"gitt\". Did you mean git, gitg or gist?";
+    const BREW_INSTALL_NO_ARGUMENT: &str =
+        "Install a formula or cask. Additional options specific to a formula may be";
+    const BREW_ALREADY_INSTALLED: &str = "Warning: git-2.3.5 already installed";
+
     macro_rules! parameterized_match_rule_tests {
-        ($($name:ident: $value:expr,)*) => {
+        ($match_rule:expr, $($name:ident: $value:expr,)*) => {
             $(
                 #[test]
                 fn $name() {
@@ -61,14 +71,14 @@ mod tests {
                                 Some(stdout.to_owned()),
                                 None
                             );
-                    assert!(match_rule(&mut command, None));
+                    assert!($match_rule(&mut command, None));
                 }
             )*
         }
     }
 
     macro_rules! parameterized_unmatch_rule_tests {
-        ($($name:ident: $value:expr,)*) => {
+        ($match_rule:expr, $($name:ident: $value:expr,)*) => {
             $(
                 #[test]
                 fn $name() {
@@ -78,43 +88,62 @@ mod tests {
                                 Some(stdout.to_owned()),
                                 None
                             );
-                    assert!(!match_rule(&mut command, None));
+                    assert!(!$match_rule(&mut command, None));
                 }
             )*
         }
     }
 
     macro_rules! parameterized_get_new_command_tests {
-        ($($name:ident: $value:expr,)*) => {
+        ($get_new_command:expr, $($name:ident: $value:expr,)*) => {
             $(
                 #[test]
                 fn $name() {
-                    let (script, stdout, expected) = $value;
+                    let (script, stdout, expected, neq) = $value;
                     let mut command = CrabCommand::new(
                                 script.to_owned(),
                                 Some(stdout.to_owned()),
                                 None
                             );
-                    assert_eq!(get_new_command(&mut command, None)[0], expected);
+                    if neq{
+                        assert_ne!($get_new_command(&mut command, None), expected);
+                    }
+                    else{
+                        assert_eq!($get_new_command(&mut command, None), expected);
+                    }
                 }
             )*
         }
     }
 
     parameterized_match_rule_tests! {
-        match_rule_1: ("cd foo", "cd: foo: No such file or directory"),
-        match_rule_2: ("cd foo/bar/baz", "cd: foo: No such file or directory"),
-        match_rule_3: ("cd foo/bar/baz", "cd: can't cd to foo/bar/baz"),
-        match_rule_4: ("cd /foo/bar/", "cd: The directory \"/foo/bar/\" does not exist"),
+        match_rule,
+        match_rule_1: ("brew install giss", BREW_NO_AVAILABLE_FORMULA_ONE),
+        match_rule_2: ("brew install elasticserar", BREW_NO_AVAILABLE_FORMULA_TWO),
+        match_rule_3: ("brew install gitt", BREW_NO_AVAILABLE_FORMULA_THREE),
     }
 
     parameterized_unmatch_rule_tests! {
-        unmatch_rule_1: ("cd foo", ""),
-        unmatch_rule_2: ("", ""),
+        match_rule,
+        unmatch_rule_1: ("brew install git", BREW_ALREADY_INSTALLED),
+        unmatch_rule_2: ("brew install", BREW_INSTALL_NO_ARGUMENT),
     }
 
     parameterized_get_new_command_tests! {
-        get_new_command_1: ("cd foo", "", "mkdir -p foo && cd foo"),
-        get_new_command_2: ("cd foo/bar/baz", "", "mkdir -p foo/bar/baz && cd foo/bar/baz"),
+        get_new_command,
+        get_new_command_1: ("brew install giss", BREW_NO_AVAILABLE_FORMULA_ONE, ["brew install gist"], false),
+        get_new_command_2: ("brew install elasticsear", BREW_NO_AVAILABLE_FORMULA_TWO, ["brew install elasticsearch", "brew install elasticsearch@6"], false),
+        get_new_command_3: ("brew install gitt", BREW_NO_AVAILABLE_FORMULA_THREE, ["brew install git", "brew install gitg", "brew install gist"], false),
+        get_new_command_4: ("brew install aa", BREW_NO_AVAILABLE_FORMULA_ONE, ["brew install aha"], true),
+    }
+
+    #[test]
+    fn test_suggestions() {
+        assert_eq!(get_suggestions("one".to_owned()), ["one"]);
+        assert_eq!(get_suggestions("one or two".to_owned()), ["one", "two"]);
+        assert_eq!(
+            get_suggestions("one, two or three".to_owned()),
+            ["one", "two", "three"]
+        );
     }
 }
