f24f3704bebf3d600d88f397e7f9aa7525cbfdb5
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-02-07 21:39:29 +0100

    Add unit tests

--- src/rules/git_checkout.rs
+++ src/rules/git_checkout.rs
@@ -7,16 +7,20 @@ use crate::{
 };
 use regex::Regex;
 
-use std::process::Command;
 use std::str;
+use std::process::Command;
 
-fn get_branches() -> Vec<String> {
-    let output = Command::new("git")
-        .args(&["branch", "-a", "--no-color", "--no-column"])
-        .output()
-        .expect("Failed to execute command");
-
-    let stdout = str::from_utf8(&output.stdout).unwrap();
+fn get_branches(mock_output: Option<&str>) -> Vec<String> {
+    let mut stdout: String;
+    if let Some(mock_output_str) = mock_output {
+        stdout = mock_output_str.to_owned();
+    } else {
+        let output = Command::new("git")
+            .args(&["branch", "-a", "--no-color", "--no-column"])
+            .output()
+            .expect("Failed to execute command");
+        stdout = str::from_utf8(&output.stdout).unwrap().to_owned();
+    }
 
     let mut branches = Vec::new();
 
@@ -41,20 +45,20 @@ fn get_branches() -> Vec<String> {
 fn auxiliary_match_rule(command: &CrabCommand) -> bool {
     if let Some(stdout) = &command.stdout {
         stdout.contains("did not match any file(s) known to git")
-            && stdout.contains("Did you forget to 'git add'?")
+            && !stdout.contains("Did you forget to 'git add'?")
     } else {
         false
     }
 }
 
 pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
-    println!("{:?}", command);
     match_rule_with_git_support(auxiliary_match_rule, command)
 }
 
-fn auxiliary_get_new_command(
+fn mockable_get_new_command(
     command: &CrabCommand,
     system_shell: Option<&dyn Shell>,
+    mock_output: Option<&str>,
 ) -> Vec<String> {
     if let Some(stdout) = &command.stdout {
         let re = Regex::new(r"error: pathspec '([^']*)' did not match any file\(s\) known to git")
@@ -62,7 +66,7 @@ fn auxiliary_get_new_command(
         if let Some(caps) = re.captures(stdout) {
             let missing_file = caps.get(1).map_or("", |m| m.as_str());
 
-            let branches = get_branches();
+            let branches = get_branches(mock_output);
             let branches: Vec<&str> = branches.iter().map(|s| s.as_str()).collect();
             let closest_branch = get_closest(missing_file, &branches, None, false);
 
@@ -94,6 +98,14 @@ fn auxiliary_get_new_command(
     }
 }
 
+
+fn auxiliary_get_new_command(
+    command: &CrabCommand,
+    system_shell: Option<&dyn Shell>,
+) -> Vec<String> {
+    mockable_get_new_command(command, system_shell, None)
+}
+
 pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
     get_command_with_git_support(auxiliary_get_new_command, command, system_shell)
 }
@@ -112,7 +124,7 @@ pub fn get_rule() -> Rule {
 
 #[cfg(test)]
 mod tests {
-    use super::{get_new_command, match_rule};
+    use super::{mockable_get_new_command, match_rule, get_branches};
     use crate::cli::command::CrabCommand;
     use crate::shell::Bash;
 
@@ -139,6 +151,32 @@ mod tests {
         let crab_command = &mut CrabCommand::new(command.to_owned(), Some(output), None);
         assert!(match_rule(crab_command, None));
     }
+
+    #[rstest]
+    #[case("git submodule update unknown", did_not_match("unknown", true))]
+    #[case("git checkout known", "")]
+    #[case("git commit known", "")]
+    fn test_not_match(#[case] command: &str, #[case] output: String) {
+        let crab_command = &mut CrabCommand::new(command.to_owned(), Some(output), None);
+        assert!(!match_rule(crab_command, None));
+    }
+
+    #[rstest]
+    #[case("", vec![])]
+    #[case("* master", vec!["master"])]
+    #[case("  remotes/origin/master", vec!["master"])]
+    #[case("  remotes/origin/test/1", vec!["test/1"])]
+    #[case("  remotes/origin/test/1/2/3", vec!["test/1/2/3"])]
+    #[case("  test/1", vec!["test/1"])]
+    #[case("  test/1/2/3", vec!["test/1/2/3"])]
+    #[case("  remotes/origin/HEAD -> origin/master", vec![])]
+    #[case("  just-another-branch", vec!["just-another-branch"])]
+    #[case("* master\n  just-another-branch", vec!["master", "just-another-branch"])]
+    #[case("* master\n  remotes/origin/master\n  just-another-branch", vec!["master", "master", "just-another-branch"])]
+    fn test_get_branches(#[case] branches: String, #[case] branch_list: Vec<&str>) {
+        assert_eq!(get_branches(Some(&branches)), branch_list)
+    }
+
 
     // #[rstest]
     // #[case("git submodule update unknown", did_not_match("unknown", true))]
