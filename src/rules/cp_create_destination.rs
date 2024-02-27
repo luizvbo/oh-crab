b215bf604a2cb58c0fea7d93a87f4e2b26c24d38
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-02-27 19:37:39 +0100

    Update rule

--- src/rules/cp_create_destination.rs
+++ src/rules/cp_create_destination.rs
@@ -1,29 +1,32 @@
+use super::{utils::match_rule_with_is_app, Rule};
 use crate::{cli::command::CrabCommand, shell::Shell};
-use regex::Regex;
 
-use super::{get_new_command_without_sudo, match_without_sudo, Rule};
-
-pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
-    if let Some(stdout) = &command.stdout {
-        stdout.contains("No such file or directory") || 
-        stdout.starts_with("cp: directory") &&
-        stdout.trim().ends_with("does not exist")
-    }
-    else {
+pub fn auxiliary_match_rule(command: &CrabCommand) -> bool {
+    if let Some(output) = &command.output {
+        println!("{:?}", command);
+        println!("{:?}", output.contains("No such file or directory"));
+        output.contains("No such file or directory")
+            || (output.starts_with("cp: directory")
+                && output.trim_end().ends_with("does not exist"))
+    } else {
         false
     }
 }
 
+pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
+    match_rule_with_is_app(auxiliary_match_rule, command, vec!["cp", "mv"], None)
+}
 
 pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
-    // TODO: Add and method
-    system_shell.unwrap().and
-        // .and_(u"mkdir -p {}".format(command.script_parts[-1]), command.script)
+    vec![system_shell.unwrap().and(vec![
+        &format!("mkdir -p {}", command.script_parts.last().unwrap()),
+        &command.script,
+    ])]
 }
 
 pub fn get_rule() -> Rule {
     Rule::new(
-        "cd_mkdir".to_owned(),
+        "cp_create_destination".to_owned(),
         None,
         None,
         None,
@@ -37,72 +40,42 @@ pub fn get_rule() -> Rule {
 mod tests {
     use super::{get_new_command, match_rule};
     use crate::cli::command::CrabCommand;
+    use crate::shell::Bash;
+    use rstest::rstest;
 
-    macro_rules! parameterized_match_rule_tests {
-        ($($name:ident: $value:expr,)*) => {
-            $(
-                #[test]
-                fn $name() {
-                    let (script, stdout) = $value;
-                    let mut command = CrabCommand::new(
-                                script.to_owned(),
-                                Some(stdout.to_owned()),
-                                None
-                            );
-                    assert!(match_rule(&mut command, None));
-                }
-            )*
-        }
+    #[rstest]
+    #[case("cp", "cp: directory foo does not exist\n", true)]
+    #[case("mv", "No such file or directory", true)]
+    #[case("cp", "", false)]
+    #[case("mv", "", false)]
+    #[case("ls", "No such file or directory", false)]
+    fn test_match(#[case] script: &str, #[case] output: &str, #[case] is_match: bool) {
+        let mut command = CrabCommand::new(script.to_owned(), Some(output.to_owned()), None);
+        assert_eq!(match_rule(&mut command, None), is_match);
     }
 
-    macro_rules! parameterized_unmatch_rule_tests {
-        ($($name:ident: $value:expr,)*) => {
-            $(
-                #[test]
-                fn $name() {
-                    let (script, stdout) = $value;
-                    let mut command = CrabCommand::new(
-                                script.to_owned(),
-                                Some(stdout.to_owned()),
-                                None
-                            );
-                    assert!(!match_rule(&mut command, None));
-                }
-            )*
-        }
-    }
-
-    macro_rules! parameterized_get_new_command_tests {
-        ($($name:ident: $value:expr,)*) => {
-            $(
-                #[test]
-                fn $name() {
-                    let (script, stdout, expected) = $value;
-                    let mut command = CrabCommand::new(
-                                script.to_owned(),
-                                Some(stdout.to_owned()),
-                                None
-                            );
-                    assert_eq!(get_new_command(&mut command, None)[0], expected);
-                }
-            )*
-        }
-    }
-
-    parameterized_match_rule_tests! {
-        match_rule_1: ("cd foo", "cd: foo: No such file or directory"),
-        match_rule_2: ("cd foo/bar/baz", "cd: foo: No such file or directory"),
-        match_rule_3: ("cd foo/bar/baz", "cd: can't cd to foo/bar/baz"),
-        match_rule_4: ("cd /foo/bar/", "cd: The directory \"/foo/bar/\" does not exist"),
-    }
-
-    parameterized_unmatch_rule_tests! {
-        unmatch_rule_1: ("cd foo", ""),
-        unmatch_rule_2: ("", ""),
-    }
-
-    parameterized_get_new_command_tests! {
-        get_new_command_1: ("cd foo", "", "mkdir -p foo && cd foo"),
-        get_new_command_2: ("cd foo/bar/baz", "", "mkdir -p foo/bar/baz && cd foo/bar/baz"),
+    #[rstest]
+    #[case(
+        "cp foo bar/",
+        "cp: directory foo does not exist\n",
+        "mkdir -p bar/ && cp foo bar/"
+    )]
+    #[case(
+        "mv foo bar/",
+        "No such file or directory",
+        "mkdir -p bar/ && mv foo bar/"
+    )]
+    #[case(
+        "cp foo bar/baz/",
+        "cp: directory foo does not exist\n",
+        "mkdir -p bar/baz/ && cp foo bar/baz/"
+    )]
+    fn test_get_new_command(#[case] script: &str, #[case] output: &str, #[case] expected: &str) {
+        let system_shell = Bash {};
+        let mut command = CrabCommand::new(script.to_owned(), Some(output.to_owned()), None);
+        assert_eq!(
+            get_new_command(&mut command, Some(&system_shell)),
+            vec![expected]
+        );
     }
 }
