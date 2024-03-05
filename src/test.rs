59041faf97a870522bb74ec130eb3e7690ee30a1
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-03-05 23:31:37 +0100

    Use tokio BufReader to check when to kill the program

--- src/test.rs
+++ src/test.rs
@@ -3,21 +3,21 @@ mod tests {
     use crate::ENV_VAR_NAME_SHELL;
     use regex::Regex;
     use rstest::rstest;
-    use core::panic;
     use std::env;
-    use std::process::{Command, Stdio};
+    use std::process::Stdio;
     use std::str;
-    use std::thread;
-    use std::time::Duration;
+    use tokio::io::{AsyncBufReadExt, BufReader};
+    use tokio::process::Command as TokioCommand;
 
     #[rstest]
     #[case("cd abcdef", "mkdir -p abcdef && cd abcdef")]
-    fn test_match(
+    #[tokio::test]
+    async fn test_match(
         #[case] command: &str,
         #[case] expected_output: &str,
     ) {
         env::set_var(ENV_VAR_NAME_SHELL, "bash");
-        let mut child = Command::new("cargo")
+        let mut child = TokioCommand::new("cargo")
             .arg("run")
             .arg("--")
             .arg(command)
@@ -25,20 +25,25 @@ mod tests {
             .spawn()
             .expect("Command failed to start");
 
-        thread::sleep(Duration::from_secs(1));
-        child
-            .kill()
-            .expect("Failed to send SIGINT to child process");
+        let reader = BufReader::new(child.stdout.take().expect("child did not have a handle to stdout"));
+        let mut lines = reader.lines();
+        let mut command_line = "".to_owned();
 
-        let output = child.wait_with_output().expect("Failed to wait on child");
-        let stdout = str::from_utf8(&output.stdout).unwrap();
+        while let Some(line) = lines.next_line().await.expect("Failed to read line from child stdout") {
+            if line.starts_with("Candidate command(s): [") {
+                command_line = line.clone();
+                child.kill().await.expect("Failed to send SIGINT to child process");
+                break;
+            }
+        }
 
-        let re = Regex::new(r#"\nCandidate command\(s\): \["(.*?)"\]"#).unwrap();
-        if let Some(caps) = re.captures(stdout) {
+        let re = Regex::new(r#"Candidate command\(s\): \["(.*)"\]"#).unwrap();
+        println!("{:?}", re.captures(&command_line));
+        if let Some(caps) = re.captures(&command_line) {
             assert_eq!(caps.get(1).unwrap().as_str(), expected_output);
         }
         else {
-            panic!("It was not possible to get the command generated from: \"{}\"", stdout)
+            panic!("It was not possible to get the command from the line: \"{}\"", command_line);
         }
     }
 }
