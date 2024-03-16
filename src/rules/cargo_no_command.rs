07e15e5ef94c5c6c24bd03ee86bee51977bdd25d
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-03-16 10:29:25 +0100

    Update rule

--- src/rules/cargo_no_command.rs
+++ src/rules/cargo_no_command.rs
@@ -1,34 +1,35 @@
-use regex::Regex;
-
-use super::{utils::match_rule_with_is_app, Rule};
 use crate::{cli::command::CrabCommand, shell::Shell};
+use super::{get_new_command_without_sudo, match_rule_with_is_app, Rule};
+use regex::Regex;
 
 fn auxiliary_match_rule(command: &CrabCommand) -> bool {
     if let Some(output) = &command.output {
-        output.to_lowercase().contains("no such subcommand") && output.contains("Did you mean")
+        command.script_parts.get(1).is_some() && output.to_lowercase().contains("no such subcommand") && output.contains("Did you mean")
     } else {
         false
     }
 }
 
 pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
-    match_rule_with_is_app(auxiliary_match_rule, command, vec!["cargo"], None)
+    match_rule_with_is_app(auxiliary_match_rule, command, vec!["cargo"], Some(1))
+}
+
+pub fn auxiliary_get_new_command(command: &CrabCommand) -> Vec<String> {
+    let re = Regex::new(r"Did you mean `([^`]*)`").unwrap();
+    let broken = command.script_parts[1].clone();
+    let fix = re.captures(&command.output.as_ref().unwrap()).unwrap().get(1).map_or("", |m| m.as_str());
+    vec![command.script.replace(&broken, fix)]
 }
 
 pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
-    if command.script_parts.len() > 1 {
-        let broken = command.script_parts[1];
-        let re = Regex::new(r"Did you mean `([^`]*)`").unwrap();
-    } else {
-        Vec::<String>::new()
-    }
+    get_new_command_without_sudo(auxiliary_get_new_command, command)
 }
 
 pub fn get_rule() -> Rule {
     Rule::new(
-        "dry".to_owned(),
+        "cargo_no_command".to_owned(),
         None,
-        Some(900),
+        None,
         None,
         match_rule,
         get_new_command,
@@ -42,22 +43,21 @@ mod tests {
     use crate::cli::command::CrabCommand;
     use rstest::rstest;
 
+    const NO_SUCH_SUBCOMMAND_OLD: &str = "No such subcommand\n\n        Did you mean `build`?\n";
+    const NO_SUCH_SUBCOMMAND: &str = "error: no such subcommand\n\n\tDid you mean `build`?\n";
+
     #[rstest]
-    #[case("cd cd foo", "", true)]
-    #[case("git git push origin/master", "", true)]
+    #[case("cargo buid", NO_SUCH_SUBCOMMAND_OLD, true)]
+    #[case("cargo buils", NO_SUCH_SUBCOMMAND, true)]
     fn test_match(#[case] command: &str, #[case] stdout: &str, #[case] is_match: bool) {
         let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
         assert_eq!(match_rule(&mut command, None), is_match);
     }
 
     #[rstest]
-    #[case("cd cd foo", "", vec!["cd foo"])]
-    #[case("git git push origin/master", "", vec!["git push origin/master"])]
-    fn test_get_new_command(
-        #[case] command: &str,
-        #[case] stdout: &str,
-        #[case] expected: Vec<&str>,
-    ) {
+    #[case("cargo buid", NO_SUCH_SUBCOMMAND_OLD, vec!["cargo build"])]
+    #[case("cargo buils", NO_SUCH_SUBCOMMAND, vec!["cargo build"])]
+    fn test_get_new_command(#[case] command: &str, #[case] stdout: &str, #[case] expected: Vec<&str>) {
         let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
         assert_eq!(get_new_command(&mut command, None), expected);
     }
