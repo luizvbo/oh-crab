430299590398fe2927d3274a6eb62dcf56273f4f
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-03-25 13:52:12 +0100

    Fix issues

--- src/rules/gradle_wrapper.rs
+++ src/rules/gradle_wrapper.rs
@@ -1,40 +1,50 @@
-use super::{get_new_command_without_sudo, utils::match_rule_with_is_app, Rule};
+use super::{utils::match_rule_with_is_app, Rule};
 use crate::{cli::command::CrabCommand, shell::Shell};
+use std::path::Path;
+use which::which;
 
-fn auxiliary_match_rule(command: &CrabCommand) -> bool {
+fn file_exists(path: &str) -> bool {
+    Path::new("gradlew").exists()
+}
+
+fn is_terminal_command(terminal_command: &str) -> bool {
+    which(terminal_command).is_ok()
+}
+
+fn auxiliary_match_rule<F, G>(
+    command: &CrabCommand,
+    fn_is_terminal_command: F,
+    fn_file_exists: G,
+) -> bool
+where
+    F: Fn(&str) -> bool,
+    G: Fn(&str) -> bool,
+{
     if let Some(output) = &command.output {
-        output.contains("image is being used by running container")
+        !fn_is_terminal_command(&command.script_parts[0])
+            && output.contains("not found")
+            && fn_file_exists("gradlew")
     } else {
         false
     }
 }
 
 pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
-    match_rule_with_is_app(auxiliary_match_rule, command, vec!["docker"], None)
-}
-
-pub fn auxiliary_get_new_command(command: &CrabCommand) -> Vec<String> {
-    let container_id = command
-        .output
-        .as_ref()
-        .unwrap()
-        .trim()
-        .split(' ')
-        .last()
-        .unwrap();
-    vec![format!(
-        "docker container rm -f {} && {}",
-        container_id, command.script
-    )]
+    match_rule_with_is_app(
+        |command| auxiliary_match_rule(command, is_terminal_command, file_exists),
+        command,
+        vec!["gradle"],
+        None,
+    )
 }
 
 pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
-    get_new_command_without_sudo(auxiliary_get_new_command, command)
+    vec![format!("./gradlew {}", command.script_parts[1..].join(" "))]
 }
 
 pub fn get_rule() -> Rule {
     Rule::new(
-        "docker_image_being_used_by_container".to_owned(),
+        "gradle_wrapper".to_owned(),
         None,
         None,
         None,
@@ -43,34 +53,50 @@ pub fn get_rule() -> Rule {
         None,
     )
 }
+
 #[cfg(test)]
 mod tests {
-    use super::{get_new_command, match_rule};
+    use super::{auxiliary_match_rule, get_new_command};
     use crate::cli::command::CrabCommand;
+    use crate::rules::utils::match_rule_with_is_app;
+    use crate::shell::Bash;
     use rstest::rstest;
 
-    const ERR_RESPONSE: &str = "Error response from daemon: conflict: unable to delete cd809b04b6ff (cannot be forced) - image is being used by running container e5e2591040d1";
-
     #[rstest]
-    #[case("docker image rm -f cd809b04b6ff", ERR_RESPONSE, true)]
-    #[case(
-        "docker image rm -f cd809b04b6ff",
-        "bash: docker: command not found",
-        false
-    )]
-    #[case("git image rm -f cd809b04b6ff", ERR_RESPONSE, false)]
-    fn test_match(#[case] command: &str, #[case] stdout: &str, #[case] is_match: bool) {
+    #[case("gradle tasks", "gradle: not found", false, true, true)]
+    #[case("gradle build", "gradle: not found", false, true, true)]
+    #[case("gradle tasks", "gradle: not found", false, false, false)]
+    #[case("gradle tasks", "command not found", true, true, false)]
+    #[case("npm tasks", "npm: not found", false, true, false)]
+    fn test_match(
+        #[case] command: &str,
+        #[case] stdout: &str,
+        #[case] is_terminal_command: bool,
+        #[case] file_exists: bool,
+        #[case] is_match: bool,
+    ) {
         let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
-        assert_eq!(match_rule(&mut command, None), is_match);
+        assert_eq!(
+            match_rule_with_is_app(
+                |command| auxiliary_match_rule(command, |s| is_terminal_command, |s| file_exists),
+                &command,
+                vec!["gradle"],
+                None,
+            ),
+            is_match
+        );
     }
 
     #[rstest]
-    #[case("docker image rm -f cd809b04b6ff", ERR_RESPONSE, vec!["docker container rm -f e5e2591040d1 && docker image rm -f cd809b04b6ff"])]
+    #[case("gradle assemble", "", vec!["./gradlew assemble"])]
+    #[case("gradle --help", "", vec!["./gradlew --help"])]
+    #[case("gradle build -c", "", vec!["./gradlew build -c"])]
     fn test_get_new_command(
         #[case] command: &str,
         #[case] stdout: &str,
         #[case] expected: Vec<&str>,
     ) {
+        let system_shell = Bash {};
         let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
         assert_eq!(get_new_command(&mut command, None), expected);
     }
