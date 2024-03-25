2ebc20016d28759806d9f5d4fac7816b4d1351ad
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-03-25 17:24:15 +0100

    Add unit tests

--- src/rules/mvn_unknown_lifecycle_phase.rs
+++ src/rules/mvn_unknown_lifecycle_phase.rs
@@ -1,42 +1,58 @@
-use super::{ get_new_command_without_sudo, match_rule_without_sudo, utils::match_rule_with_is_app, Rule, };
-use crate::{cli::command::CrabCommand, shell::Shell};
+use super::{utils::match_rule_with_is_app, Rule};
+use crate::{
+    cli::command::CrabCommand,
+    shell::Shell,
+    utils::{get_close_matches, replace_command},
+};
 use regex::Regex;
 
-fn get_failed_lifecycle(command: &CrabCommand) -> Option<String> {
-    let re = Regex::new(r"\[ERROR\] Unknown lifecycle phase \"(.+)\"").unwrap();
-    re.captures(&command.output).and_then(|caps| caps.get(1).map(|m| m.as_str().to_owned()))
+fn get_failed_lifecycle(command_output: &str) -> Option<String> {
+    let re = Regex::new(r#"\[ERROR\] Unknown lifecycle phase \"(.+)\""#).unwrap();
+    if let Some(caps) = re.captures(command_output) {
+        Some(caps[1].to_string())
+    } else {
+        None
+    }
 }
 
-fn getavailable_lifecycles(command: &CrabCommand) -> Option<Vec<String>> {
+fn getavailable_lifecycles(command_output: &str) -> Option<String> {
     let re = Regex::new(r"Available lifecycle phases are: (.+) -> \[Help 1\]").unwrap();
-    re.captures(&command.output).and_then(|caps| caps.get(1).map(|m| m.as_str().split(", ").map(|s| s.to_owned()).collect()))
+    if let Some(caps) = re.captures(command_output) {
+        Some(caps[1].to_string())
+    } else {
+        None
+    }
 }
 
 fn auxiliary_match_rule(command: &CrabCommand) -> bool {
     if let Some(output) = &command.output {
-        command.script_parts.first().map_or(false, |s| s == "mvn") && get_failed_lifecycle(command).is_some() && getavailable_lifecycles(command).is_some()
+        get_failed_lifecycle(output).is_some() && getavailable_lifecycles(output).is_some()
     } else {
         false
     }
 }
 
 pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
-    match_rule_without_sudo( |command| match_rule_with_is_app(auxiliary_match_rule, command, vec!["mvn"], None), command,)
-}
-
-pub fn auxiliary_get_new_command(command: &CrabCommand) -> Vec<String> {
-    let failed_lifecycle = get_failed_lifecycle(command);
-    let available_lifecycles = getavailable_lifecycles(command);
-    if let (Some(failed_lifecycle), Some(available_lifecycles)) = (failed_lifecycle, available_lifecycles) {
-        let selected_lifecycle = available_lifecycles.iter().min_by_key(|a| levenshtein::levenshtein(a, &failed_lifecycle)).unwrap_or(&"".to_string()).to_owned();
-        vec![command.script.replace(&failed_lifecycle, &selected_lifecycle)]
-    } else {
-        vec![]
-    }
+    match_rule_with_is_app(auxiliary_match_rule, command, vec!["mvn"], None)
 }
 
 pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
-    get_new_command_without_sudo(auxiliary_get_new_command, command)
+    if let Some(output) = &command.output {
+        let failed_lifecycle = get_failed_lifecycle(output);
+        let available_lifecycles = getavailable_lifecycles(output);
+        if let (Some(failed_lifecycle), Some(available_lifecycles)) =
+            (failed_lifecycle, available_lifecycles)
+        {
+            let available_lifecycles: Vec<&str> = available_lifecycles.split(", ").collect();
+            let selected_lifecycle =
+                get_close_matches(&failed_lifecycle, &available_lifecycles, None, None);
+            replace_command(command, &failed_lifecycle, selected_lifecycle)
+        } else {
+            vec![]
+        }
+    } else {
+        vec![]
+    }
 }
 
 pub fn get_rule() -> Rule {
@@ -79,4 +95,20 @@ mod tests {
     #[case("mvn clean", MVN_CLEAN, false)]
     #[case("mvn --help", "", false)]
     #[case("mvn -v", "", false)]
-    fn test_match
+    fn test_match(#[case] command: &str, #[case] stdout: &str, #[case] is_match: bool) {
+        let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
+        assert_eq!(match_rule(&mut command, None), is_match);
+    }
+    #[rstest]
+    #[case("mvn -N", r#"[ERROR] Unknown lifecycle phase "cle". You must specify a valid lifecycle phase or a goal in the format <plugin-prefix>:<goal> or <plugin-group-id>:<plugin-artifact-id>[:<plugin-version>]:<goal>. Available lifecycle phases are: validate, initialize, generate-sources, process-sources, generate-resources, process-resources, compile, process-classes, generate-test-sources, process-test-sources, generate-test-resources, process-test-resources, test-compile, process-test-classes, test, prepare-package, package, pre-integration-test, integration-test, post-integration-test, verify, install, deploy, pre-clean, clean, post-clean, pre-site, site, post-site, site-deploy. -> [Help 1]"#, vec!["mvn clean", "mvn compile"])]
+    #[case("mvn claen package", r#"[ERROR] Unknown lifecycle phase "claen". You must specify a valid lifecycle phase or a goal in the format <plugin-prefix>:<goal> or <plugin-group-id>:<plugin-artifact-id>[:<plugin-version>]:<goal>. Available lifecycle phases are: validate, initialize, generate-sources, process-sources, generate-resources, process-resources, compile, process-classes, generate-test-sources, process-test-sources, generate-test-resources, process-test-resources, test-compile, process-test-classes, test, prepare-package, package, pre-integration-test, integration-test, post-integration-test, verify, install, deploy, pre-clean, clean, post-clean, pre-site, site, post-site, site-deploy. -> [Help 1]"#, vec!["mvn clean package"])]
+    fn test_get_new_command(
+        #[case] command: &str,
+        #[case] stdout: &str,
+        #[case] expected: Vec<&str>,
+    ) {
+        let system_shell = Bash {};
+        let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
+        assert_eq!(get_new_command(&mut command, None), expected);
+    }
+}
