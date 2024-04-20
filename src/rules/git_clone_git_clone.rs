fcdec0edba6ba49cd50ebcf3a9134bc1508d8092
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-04-20 12:32:36 +0200

    FEAT: Add rule: git clone git clone (#146)

--- src/rules/git_clone_git_clone.rs
+++ src/rules/git_clone_git_clone.rs
@@ -5,27 +5,25 @@ use crate::{
         Rule,
     },
     shell::Shell,
-    utils::replace_argument,
 };
 
 fn auxiliary_match_rule(command: &CrabCommand) -> bool {
-    if let Some(stdout) = &command.output {
-        stdout.contains("Use -f if you really want to add them.")
-            && command.script_parts.contains(&"add".to_owned())
+    if let Some(output) = &command.output {
+        command.script.contains(" git clone ") && output.contains("fatal: Too many arguments.")
     } else {
         false
     }
 }
 
-pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
+pub fn match_rule(command: &mut CrabCommand, _system_shell: Option<&dyn Shell>) -> bool {
     match_rule_with_git_support(auxiliary_match_rule, command)
 }
 
 fn auxiliary_get_new_command(
     command: &CrabCommand,
-    system_shell: Option<&dyn Shell>,
+    _system_shell: Option<&dyn Shell>,
 ) -> Vec<String> {
-    vec![replace_argument(&command.script, "add", "add --force")]
+    vec![command.script.replacen(" git clone ", " ", 1)]
 }
 
 pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> Vec<String> {
@@ -34,7 +32,7 @@ pub fn get_new_command(command: &mut CrabCommand, system_shell: Option<&dyn Shel
 
 pub fn get_rule() -> Rule {
     Rule::new(
-        "git_add_force".to_owned(),
+        "git_clone_git_clone".to_owned(),
         None,
         None,
         None,
@@ -51,22 +49,21 @@ mod tests {
     use crate::shell::Bash;
     use rstest::rstest;
 
-    const OUTPUT: &str = "The following paths are ignored by one of your .gitignore files:\n\
-dist/app.js\n\
-dist/background.js\n\
-dist/options.js\n\
-Use -f if you really want to add them.\n";
+    const OUTPUT_CLEAN: &str = "\
+fatal: Too many arguments.\n\n\
+usage: git clone [<options>] [--] <repo> [<dir>]\n";
 
     #[rstest]
-    #[case("git add dist/*.js", OUTPUT, true)]
-    #[case("git add dist/*.js", "", false)]
+    #[case("git clone git clone foo", OUTPUT_CLEAN, true)]
+    #[case("git clone foo", "", false)]
+    #[case("git clone foo bar baz", OUTPUT_CLEAN, false)]
     fn test_match(#[case] command: &str, #[case] stdout: &str, #[case] is_match: bool) {
         let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
         assert_eq!(match_rule(&mut command, None), is_match);
     }
 
     #[rstest]
-    #[case("git add dist/*.js", OUTPUT, vec!["git add --force dist/*.js"])]
+    #[case("git clone git clone foo", OUTPUT_CLEAN, vec!["git clone foo"])]
     fn test_get_new_command(
         #[case] command: &str,
         #[case] stdout: &str,
@@ -74,6 +71,9 @@ Use -f if you really want to add them.\n";
     ) {
         let system_shell = Bash {};
         let mut command = CrabCommand::new(command.to_owned(), Some(stdout.to_owned()), None);
-        assert_eq!(get_new_command(&mut command, None), expected);
+        assert_eq!(
+            get_new_command(&mut command, Some(&system_shell)),
+            expected.iter().map(|&s| s.to_owned()).collect::<Vec<_>>()
+        );
     }
 }
