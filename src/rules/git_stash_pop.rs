cb8825853781098a76fd81657fe4877004c540ac
Luiz Otavio Vilas Boas Oliveira <luiz.vbo@gmail.com>
2024-04-29 12:50:50 +0200

    Add rule

--- src/rules/git_stash_pop.rs
+++ src/rules/git_stash_pop.rs
@@ -1,41 +1,46 @@
 use crate::{
     cli::command::CrabCommand,
-    rules::{match_rule_with_git_support, utils::git::get_new_command_with_git_support},
-    Rule,
+    rules::{
+        utils::git::{get_new_command_with_git_support, match_rule_with_git_support},
+        Rule,
+    },
+    shell::Shell,
 };
-use shell::Shell;
 
 fn auxiliary_match_rule(command: &CrabCommand) -> bool {
-    command.script.contains("stash")
-        && command.script.contains("pop")
-        && command.output.as_ref().map_or(false, |o| {
-            o.contains("Your local changes to the following files would be overwritten by merge")
-        })
+    if let Some(stdout) = &command.output {
+        command.script.contains("stash")
+            && command.script.contains("pop")
+            && stdout
+                .contains("Your local changes to the following files would be overwritten by merge")
+    } else {
+        false
+    }
 }
 
-pub fn match_rule(command: &mut CrabCommand, _system_shell: Option<&dyn Shell>) -> bool {
+pub fn match_rule(command: &mut CrabCommand, system_shell: Option<&dyn Shell>) -> bool {
     match_rule_with_git_support(auxiliary_match_rule, command)
 }
 
 fn auxiliary_get_new_command(
     command: &CrabCommand,
-    _system_shell: Option<&dyn Shell>,
+    system_shell: Option<&dyn Shell>,
 ) -> Vec<String> {
-    vec!["git add --update && git stash pop && git reset .".to_string()]
+    vec![system_shell.unwrap().and(vec!["git add --update", "git stash pop", "git reset ."])]
 }
 
 pub fn get_new_command(
     command: &mut CrabCommand,
-    _system_shell: Option<&dyn Shell>,
+    system_shell: Option<&dyn Shell>,
 ) -> Vec<String> {
-    get_new_command_with_git_support(auxiliary_get_new_command, command, _system_shell)
+    get_new_command_with_git_support(auxiliary_get_new_command, command, system_shell)
 }
 
 pub fn get_rule() -> Rule {
     Rule::new(
         "git_stash_pop".to_owned(),
-        Some(900), // priority
         None,
+        Some(900), // priority
         None,
         match_rule,
         get_new_command,
